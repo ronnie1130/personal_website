57996ca7a11359f0a3e97d759d87d917a36f30f2
ronnie1130 <teddy871130@gmail.com>
2024-03-15 11:38:47 +0800

    information correction

--- all.js
+++ all.js
@@ -1,51 +1,53 @@
-const textEl = document.getElementById('text')
-const text = 'Hi, I’m Ronnie !'
-let idx = 1
+const textEl = document.getElementById("text");
+const text = "Hi, I’m Veronica !";
+let idx = 1;
 
-writeText()
+writeText();
 
 function writeText() {
-  textEl.innerText = text.slice(0, idx)
+  textEl.innerText = text.slice(0, idx);
 
-  idx++
+  idx++;
 
   if (idx > text.length) {
-    idx = 1
+    idx = 1;
   }
 
-  setTimeout(writeText, 300)
+  setTimeout(writeText, 300);
 }
 
-const sliderContainer = document.querySelector('.slider-container')
-const slideRight = document.querySelector('.right-slide')
-const slideLeft = document.querySelector('.left-slide')
-const upButton = document.querySelector('.up-button')
-const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelectorAll('div').length
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
 
-let activeSlideIndex = 0
+let activeSlideIndex = 0;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-upButton.addEventListener('click', () => changeSlide('up'))
-downButton.addEventListener('click', () => changeSlide('down'))
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
 
 const changeSlide = (direction) => {
-  const sliderHeight = sliderContainer.clientHeight
-  if (direction === 'up') {
-    activeSlideIndex++
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
     if (activeSlideIndex > slidesLength - 1) {
-      activeSlideIndex = 0
+      activeSlideIndex = 0;
     }
-  } else if (direction === 'down') {
-    activeSlideIndex--
+  } else if (direction === "down") {
+    activeSlideIndex--;
     if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesLength - 1
+      activeSlideIndex = slidesLength - 1;
     }
   }
 
   slideRight.style.transform = `translateY(-${
     activeSlideIndex * sliderHeight
-  }px)`
-  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-}
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
